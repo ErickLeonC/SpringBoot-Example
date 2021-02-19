3bcf3c7ff456afd417e2fd08181d839dfd663a73
ErickLeonC <65688017+ErickLeonC@users.noreply.github.com>
2021-02-18 21:30:36 -0500

    initial commit

---
+++ src/main/java/ws/synopsis/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package ws.synopsis.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import ws.synopsis.component.RequestTimeInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	@Qualifier("requestTimeInterceptor")
+	private RequestTimeInterceptor requestTimeInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestTimeInterceptor);
+	}
+
+}
